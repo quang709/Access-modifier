3c5b1cd0e322f6f02776f37c693436d37981bcc0
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-16 11:05:55 +0700

    Access modifier

---
+++ Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Access_modifier
+{
+    class Circle
+    {
+        private double Radius = 1;
+        private string Color = "red";
+
+        public Circle() { }
+
+        public Circle(double _Radius)
+        {
+            Radius = _Radius;
+        }
+        public double GetRadius()
+        {
+            return Radius;
+        }
+
+        public void SetRadius(double _Radius)
+        {
+            Radius =_Radius;
+        }
+
+
+        public string GetColor()
+        {
+            return Color;
+        }
+         public void SetColor(string _Color)
+        {
+            Color = _Color;
+        }
+    }
+}
---
+++ TestCircle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Access_modifier
+{
+    class TestCircle
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            Console.WriteLine(circle.GetColor());
+            Console.WriteLine(circle.GetRadius());
+        }
+    }
+}
